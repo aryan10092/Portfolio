24090bebb40bb624649193c6464471a6ff651fac
ARYAN GUPTA <aryan9090900@gmail.com>
2025-07-20 02:36:08 +0530

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -11,7 +11,7 @@ function Projects() {
       title: 'CodeBattle',
       description: 'A real-time competitive programming platform where users can solve challenges and compete head-to-head. Features a collaborative code editor and live chat.',
       image: codebattle,
-      tech: ['React', 'Node.js', 'Express', 'MongoDB', 'Socket.IO',],
+      tech: ['React', 'Node.js', 'Express', 'MongoDB', 'Socket.IO','Framer-motion'],
       github: 'https://github.com/aryan10092/Codebattle',
       live: 'https://codebattle-1.onrender.com/',  },
 
